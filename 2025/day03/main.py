85cf0a82912804aca834b009d300e3994b888d6e
kristiana19 <marusickristijana@gmail.com>
2025-12-02 07:18:43 +0100

    prepare for day three

---
+++ 2025/day03/main.py
@@ -0,0 +1,29 @@
+# --- Day 3 ---
+
+def parse(text: str):
+    return text
+
+
+def part1(data):
+    return 0
+
+
+def part2(data):
+    return 0
+
+
+def main():
+    with open("input.txt") as f:
+        text = f.read()
+
+    data = parse(text)
+
+    answer1 = part1(data)
+    answer2 = part2(data)
+
+    print("Part 1:", answer1)
+    print("Part 2:", answer2)
+
+
+if __name__ == "__main__":
+    main()
